f6548c3fda0a98d217cf4d9fe6cef5fcddcefb69
kadimetlaadarsh <kadimetlaadarsha@gmail.com>
2024-05-17 16:24:30 +0530

    add intial cv website files

---
+++ script.js
@@ -0,0 +1,21 @@
+/*let namasteBtn = document.querySelector('button');
+namasteBtn.addEventListener('click', inputMsg);
+
+function inputMsg(){
+    let name = prompt('enter name of student');
+    namasteBtn.textContent = 'Roll No. 1' + name;
+}
+for(let i=5;i<51;i=i+5){
+    console.log(i);
+}
+
+let animal={
+    name : "Zebra",
+    leg : 4
+};
+
+for(let key in animal){
+    console.log(key,animal[key]);
+}*/
+
+
